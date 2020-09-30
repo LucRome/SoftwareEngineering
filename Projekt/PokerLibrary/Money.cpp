b4a05a99d8805757033e748ced25751d0dcce60d
LucRome <64947931+LucRome@users.noreply.github.com>
2020-09-30 22:36:12 +0200

    Add Struct for pokerchips

---
+++ Projekt/PokerLibrary/Money.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "Money.h"
+
+chipstack& chipstack::operator+(const chipstack& a)
+{
+    chipstack result = chipstack();
+    for (int i = 0; i < m_chips.size(); i++) {
+        result.m_chips[i] = this->m_chips[i] + a.m_chips[i];
+    }
+}
---
+++ Projekt/PokerLibrary/Money.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <array>
+enum chipvalues{_5, _10, _20, _50, _100, _500};
+
+const int nr_chipvalues = 6;
+
+struct chipstack {
+	//chip values: 5, 10, 20, 50, 100, 500
+private:
+	std::array<int, nr_chipvalues> m_chips = { 0 }; //standard: all are 0
+
+public:
+	chipstack& operator+(const chipstack& a);
+};
