dedeef53016005e7c2364d3a6c51e4651bffaf1b
Sophia Schmoll <sophia.schmoll@advantest.com>
2020-10-01 08:36:32 +0200

    Menue.cpp Output and Input

--- Projekt/Projekt/Menue.cpp
+++ Projekt/Projekt/Menue.cpp
@@ -1,1 +1,21 @@
 #include "Menue.h"
+#include <iostream>
+
+
+void Menue() {
+	char displayRules, humanBot;
+	int nrPlayers;
+	std::cout << "Welcome to poker" << std::endl << "Do you whant the rules to be displayed? If yes type y" << std::endl;
+	std::cin >> displayRules;
+	if (displayRules == 'y') {
+		//call function to print rules
+		std::cout << "rules:";
+	}
+	std::cout << "Please enter the number of players: ";
+	std::cin >> nrPlayers;
+	std::cout << "Do you have friends to play with or do you whant to play against the bot? [f/b]" << std::endl;
+	std::cin >> humanBot;
+
+
+
+}
