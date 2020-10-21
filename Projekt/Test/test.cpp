0f2f7a0886f9d177eecb7cd12b5868d7c5d0fa53
LucRome <64947931+LucRome@users.noreply.github.com>
2020-10-21 22:23:36 +0200

    Test playGame()
    no real Test

--- Projekt/Test/test.cpp
+++ Projekt/Test/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "DeckOfCards.h"
+#include "GameController.h"
+#include "HumanPlayer.h"
 
 TEST(randomNumberGenerator, correctNumber) {
 	DeckOfCards deck = DeckOfCards();
@@ -23,4 +25,27 @@ TEST(randomNumberGenerator, positive) {
 		}
 	}
 	EXPECT_TRUE(pos);
+}
+
+
+//Test: GameController::playGame()
+//only while menue isnt finished
+//TODO: remove
+#define st_ch chipstack({1,1,1,1,1,1})
+#define sB chipstack({1,0,0,0,0,0})
+#define bB chipstack({2,0,0,0,0,0})
+
+TEST(playGame, justPlay) {
+	std::vector<std::shared_ptr<Player>> players;
+	players.push_back(std::make_shared<HumanPlayer>(st_ch, "A"));
+	players.push_back(std::make_shared<HumanPlayer>(st_ch, "B"));
+	players.push_back(std::make_shared<HumanPlayer>(st_ch, "C"));
+	players.push_back(std::make_shared<HumanPlayer>(st_ch, "D"));
+	players.shrink_to_fit();
+
+	GameController gc = GameController(players, 10000, bB, sB);
+	std::shared_ptr<Player>& winner = gc.playGame();
+	std::string s = winner->getName();
+	bool suc = s == "A";
+	EXPECT_TRUE(suc);
 }
