c48f2f2550962eb275d0f41c0d42a7df385e97c0
noahhauke22 <71071583+noahhauke22@users.noreply.github.com>
2020-11-05 11:42:09 +0100

    Commit 123 lul

--- Projekt/PokerLibrary/BotPlayer.cpp
+++ Projekt/PokerLibrary/BotPlayer.cpp
@@ -1,2 +1,10 @@
 #include "pch.h"
 #include "BotPlayer.h"
+
+BotPlayer::BotPlayer(chipstack pStartingChips, std::string pName) :Player(pStartingChips, pName) {
+
+}
+
+outPlay BotPlayer::play(chipstack pToCheck, std::vector<plays> pPlays) {
+
+}
--- Projekt/PokerLibrary/BotPlayer.h
+++ Projekt/PokerLibrary/BotPlayer.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Player.h"
-class BotPlayer : Player
-{
+class BotPlayer : Player{
+public:
+	BotPlayer(chipstack pStartingChips, std::string pName);
+	outPlay play(chipstack pToCheck, std::vector<plays> pPlays) override;
 };
 
