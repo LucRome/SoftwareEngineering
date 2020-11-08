9eadd80c4be9d9eb579ac1ff3f2cbaef27ef7a77
43-50ph14 <63649591+43-50ph14@users.noreply.github.com>
2020-11-08 16:20:23 +0100

    comments

--- Projekt/Projekt/Menue.cpp
+++ Projekt/Projekt/Menue.cpp
@@ -56,19 +56,20 @@ void Menue::Startgame() {
 		out.rulesOut();
 	}
 
-	// Clear Console befor game starts 
+	// Clear Console befor game starts, Comment out not needed os
 	// Windows
 	system("pause");
 	//Linux
 	//system("read");
 	out.clearConsole();
 
-
+	//Number of player will be enterd
 	do {
 		std::cout << "Please enter the number of players: " << std::endl << "The maximum number of players is 9" << std::endl;
 		nrPlayers = out.userInput();
 	} while (nrPlayers > maxPlayers || nrPlayers <= 1);
 
+	//Table range will be entered
 	do {
 		std::cout << "Please enter the table range: " << std::endl << "The minimal table range is 25000" << std::endl;
 		tableRange = out.userInput();
@@ -78,14 +79,17 @@ void Menue::Startgame() {
 	// first enterd is big blind, second small blind
 	std::cout << "The first player you enter will be big blind, the second small blind.\n";
 
+	//Create Players, Bots
 	for (int i = 0; i < nrPlayers; i++) {
 		std::cout << "Should player " << (i+1) << " be a bot or a human?" << std::endl;
 		std::cout << "1 = human; 2 = bot; 3 = smart bot" << std::endl;
 		humanBot = out.userInput();
 
+		//naming Bot or not 
 		if (humanBot != 1) {
 			std::cout << "Du you whant to name the bot? 1 = yes, 2 = no" << std::endl;
 			nameBot = out.userInput();
+			// default name for bot
 			if (nameBot == 2) {
 				std::string s = std::to_string(k);
 				playerName = "Bot" + s;
@@ -93,25 +97,29 @@ void Menue::Startgame() {
 			}
 		}
 
+		//naming players
 		if (humanBot == 1 || nameBot == 1) {
 			std::cout << "Please enter a name for player " << (i + 1) << ": " << std::endl;
 			std::cin >> playerName;
 		}
 
-
+		//chipstack and array players
 		switch (humanBot) {
 		case 1: {
 			//Human player
+			//big bilnd
 			if (i == 0) {
 				bigBlind = bigBlind.readChipstackFromConsole();
 				players.push_back(std::make_shared<HumanPlayer>(bigBlind, playerName));
 				break;
 			}
+			//small blind
 			if (i == 1) {
 				smallBlind = smallBlind.readChipstackFromConsole();
 				players.push_back(std::make_shared<HumanPlayer>(smallBlind, playerName));
 				break;
 			}
+			//other players
 			else {
 				startchips = startchips.readChipstackFromConsole();
 				players.push_back(std::make_shared<HumanPlayer>(startchips, playerName));
@@ -120,16 +128,19 @@ void Menue::Startgame() {
 		}
 		case 2: {
 			//Bot 
+			//big blind
 			if (i == 0) {
 				bigBlind = bigBlind.createChipstackForBot();
 				players.push_back(std::make_shared<DumbBot>(bigBlind, playerName));
 				break;
 			}
+			//small blind
 			if (i == 1) {
 				smallBlind = smallBlind.createChipstackForBot();
 				players.push_back(std::make_shared<DumbBot>(smallBlind, playerName));
 				break;
 			}
+			//other
 			else {
 				startchips = startchips.createChipstackForBot();
 				players.push_back(std::make_shared<DumbBot>(startchips, playerName));
@@ -164,6 +175,7 @@ void Menue::Startgame() {
 	//Connection to Game Controller 
 	players.shrink_to_fit();
 
+	//start game
 	GameController gc = GameController(players, tableRange, bigBlind, smallBlind);
 	std::shared_ptr<Player>& winner = gc.playGame();
 
